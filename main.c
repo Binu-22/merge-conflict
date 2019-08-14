11e27b3a5fd1ae86686537c410c8025879a881e8
root <root@ip-172-31-47-97.ec2.internal>
2019-08-14 02:27:03 +0000

    changed back to original

--- main.c
+++ main.c
@@ -6,22 +6,7 @@ Initial Function2()
 {
  //Sample Code
 }
-initial function 3(){
-function 2(){
-}
-}
 
-feature1()
-{
-//feature1 added
-feature2()
-{
-//feature2 added
-}
-
-security patch()
-{
-//security patch added 
 }
 
 }
