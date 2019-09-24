3f37c77b56abf17cf927a39180468a11bee22bad
root <root@ip-172-31-80-47.ec2.internal>
2019-09-24 02:37:20 +0000

    first

--- main.c
+++ main.c
@@ -2,22 +2,6 @@ pseudo code
 {
 Initial Function(){
 }
-Initial Function2()
-{
- //Sample Code
-}
 
-feature1()
-{
-//feature1 added
-}
-security patch(){
-
-
-feature2()
-{//feature2 added
-
-}
-}
 
 }
